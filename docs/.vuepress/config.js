826e1c52ff02dde87e5959d8710c622e69b8528b
YoungJun Park <youngjun108059@gmail.com>
2020-08-13 11:25:07 +0900

    [Add] Vuepress config, README.md

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,10 @@
+module.exports = {
+    title: '안녕 Veupress', // 사이트 타이틀
+    description: 'vuepress로 만든 문서입니다.',
+    themeConfig: {
+      nav: [
+        { text: 'Home', link: '/' }
+      ],
+      sidebar: 'auto' // h1~h6 같은 heading tag를 기준으로 sidebar를 만들어줌
+    }
+  }
