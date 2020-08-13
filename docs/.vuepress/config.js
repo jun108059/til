9b90b1268aa6087251ea1da63befc16d285085c1
YoungJun Park <youngjun108059@gmail.com>
2020-08-13 17:50:40 +0900

    [Update] config.js

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -9,30 +9,31 @@ module.exports = {
       ['link', { rel: 'icon', href: `/images/logo.png` }],
       ['link', { rel: 'manifest', href: '/manifest.json' }]
     ],
-    themeConfig: {
-      sidebar: [
-        sidebar.getSidebarGroup('/docker/', '🐳 Docker', true),
-        sidebar.getSidebarGroup('/kubernetes/', '🐋 Kubernetes', true),
-        sidebar.getSidebarGroup('/flutter/', '📘 Flutter', true),
-        sidebar.getSidebarGroup('/GCP/', '📗 GCP'),
-        sidebar.getSidebarGroup('/machine-learning/', '📙 Machine Learning', true),
-        sidebar.getSidebarGroup('/python/', '📕 Python', true),
-        sidebar.getSidebarGroup('/Git/', '💛 Git', true)
-      ],
-      sidebarDepth: 0,
-      nav: [
-        { text: 'GitHub', link: 'https://github.com/GimunLee/TIL' }
-      ],
-      smoothScroll: true,
-      lastUpdated: 'Last Updated'
-      // sidebar: 'auto' // h1~h6 같은 heading tag를 기준으로 sidebar를 만들어줌
-    },
-    
     plugins: [
       '@vuepress/back-to-top',
       ['@vuepress/pwa', {
         serviceWorker: true,
         updatePopup: true
       }]
-    ]
+    ],
+    themeConfig: {
+      sidebar: [
+        // sidebar.getSidebarGroup('/docker/', '🐳 Docker', true),
+        // sidebar.getSidebarGroup('/kubernetes/', '🐋 Kubernetes', true),
+        // sidebar.getSidebarGroup('/flutter/', '📘 Flutter', true),
+        // sidebar.getSidebarGroup('/GCP/', '📗 GCP'),
+        // sidebar.getSidebarGroup('/machine-learning/', '📙 Machine Learning', true),
+        // sidebar.getSidebarGroup('/python/', '📕 Python', true),
+        // sidebar.getSidebarGroup('/Git/', '💛 Git', true),
+        sidebar.getSidebarGroup('/data-strucure/', '💚 Data Structure', true)
+      ],
+      // sidebarDepth: 0,
+      sidebar: 'auto', // h1~h6 같은 heading tag를 기준으로 sidebar를 만들어줌
+      nav: [
+        { text: 'GitHub', link: 'https://github.com/jun108059/til' },
+        { text: 'Blog', link: 'https://dev-youngjun.tistory.com/' }
+      ],
+      smoothScroll: true,
+      lastUpdated: 'Last Updated'
+    }
   };
