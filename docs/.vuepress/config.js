e9eb34ac6c7ece94882d90548fcb4c8cf8d49133
YoungJun_Park <youngjun108059@gmail.com>
2020-08-16 17:04:42 +0900

    [Update] Sidebar Group

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -6,7 +6,7 @@ module.exports = {
     email: 'youngjun108059@gmail.com',
     base:'/til/',
     head: [
-      ['link', { rel: 'icon', href: `/images/logo.png` }],
+      ['link', { rel: 'icon', href: `/images/logo-144.png` }],
       ['link', { rel: 'manifest', href: '/manifest.json' }]
     ],
     plugins: [
@@ -18,14 +18,22 @@ module.exports = {
     ],
     themeConfig: {
       sidebar: [
-        // sidebar.getSidebarGroup('/docker/', '🐳 Docker', true),
-        // sidebar.getSidebarGroup('/kubernetes/', '🐋 Kubernetes', true),
-        // sidebar.getSidebarGroup('/flutter/', '📘 Flutter', true),
-        // sidebar.getSidebarGroup('/GCP/', '📗 GCP'),
-        // sidebar.getSidebarGroup('/machine-learning/', '📙 Machine Learning', true),
-        // sidebar.getSidebarGroup('/python/', '📕 Python', true),
-        // sidebar.getSidebarGroup('/Git/', '💛 Git', true),
-        sidebar.getSidebarGroup('/data-structure/', '💚 Data Structure', true)
+        sidebar.getSidebarGroup('/docker/', '🐳 Docker', true),
+        sidebar.getSidebarGroup('/kubernetes/', '🐋 Kubernetes', true),
+        sidebar.getSidebarGroup('/flutter/', '📘 Flutter', true),
+        sidebar.getSidebarGroup('/GCP/', '📗 GCP'),
+        sidebar.getSidebarGroup('/machine-learning/', '📙 Machine Learning', true),
+        sidebar.getSidebarGroup('/python/', '📕 Python', true),
+        sidebar.getSidebarGroup('/Git/', '💛 Git', true),
+        sidebar.getSidebarGroup('/data-structure/', '💚 Data Structure', true),
+        sidebar.getSidebarGroup('/Java/', '❤ Java', true),
+        sidebar.getSidebarGroup('/kubeflow/', '💙 Kubeflow', true),
+        sidebar.getSidebarGroup('/news/', '🖤 News', true),
+        sidebar.getSidebarGroup('/nginx/', '💜 NGINX', true),
+        sidebar.getSidebarGroup('/spring/', '🤍 Spring', true),
+        sidebar.getSidebarGroup('/system-programming/', '🤎 System Programming', true),
+        sidebar.getSidebarGroup('/UX&UI/', '💗 UX&UI', true),
+        sidebar.getSidebarGroup('/web/', '💕 Web', true)
       ],
       // sidebarDepth: 0,
       sidebar: 'auto', // h1~h6 같은 heading tag를 기준으로 sidebar를 만들어줌
