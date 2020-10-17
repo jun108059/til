d327b5b28ef05d10784b18386fadf5dd76615fbb
YoungJun_Park <youngjun108059@gmail.com>
2020-10-17 23:35:03 +0900

    [Update]🧱Config file

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -4,7 +4,7 @@ module.exports = {
     title: 'Today Youngjun learned✍',
     description: 'Youngjun`s Personal Wiki (Today I Learned)',
     email: 'youngjun108059@gmail.com',
-    base:"/til/",
+    base:'/til/',
     head: [
       ['link', { rel: 'icon', href: '/images/logo-144.png' }],
       ['link', { rel: 'manifest', href: '/manifest.json' }]
@@ -23,9 +23,17 @@ module.exports = {
         sidebar.getSidebarGroup('/docker/', '🐳 Docker', true),
         sidebar.getSidebarGroup('/kubernetes/', '🐋 Kubernetes', true),
         sidebar.getSidebarGroup('/flutter/', '📘 Flutter', true),
+        sidebar.getSidebarGroup('/GCP/', '📗 GCP'),
+        sidebar.getSidebarGroup('/machine-learning/', '📙 Machine Learning', true),
+        sidebar.getSidebarGroup('/Git/', '📕 Git', true),
         sidebar.getSidebarGroup('/data-structure/', '💚 Data Structure', true),
         sidebar.getSidebarGroup('/Java/', '💝 Java', true),
+        sidebar.getSidebarGroup('/kubeflow/', '💙 Kubeflow', true),
+        sidebar.getSidebarGroup('/news/', '🖤 News', true),
         sidebar.getSidebarGroup('/nginx/', '💜 NGINX', true),
+        sidebar.getSidebarGroup('/spring/', '🤍 Spring', true),
+        sidebar.getSidebarGroup('/system-programming/', '🤎 System Programming', true),
+        sidebar.getSidebarGroup('/UX&UI/', '💛 UX&UI', true),
         sidebar.getSidebarGroup('/web/', '💕 Web', true),
         sidebar.getSidebarGroup('/etc/', '📌 etc', true)
       ],
