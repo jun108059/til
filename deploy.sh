fa92d54bf0c7bde16de95c7c5a0c63a242163bb8
YoungJun Park <youngjun108059@gmail.com>
2021-02-04 00:31:07 +0900

    [Update] ✍ deploy.sh 설정 변경

--- deploy.sh
+++ deploy.sh
@@ -4,7 +4,7 @@
 set -e
 
 # 문서(md)를 build하여 html로 만든다. 
-npm run docs:build
+yarn docs:build
 
 # build가 output된 폴더로 이동한다. 
 cd docs/.vuepress/dist
@@ -12,9 +12,9 @@ cd docs/.vuepress/dist
 # init + add + commit을 해준 다음
 git init
 git add -A
-git commit -m '[Deploy]🎉Vuepress'
+git commit -m '[Deploy] Vuepress'
 
 # Deploying to https://jun108059.github.io/til
-git push -f git@github.com:jun108059/til.git master:gh-pages
+git push -f https://github.com/jun108059/til.git master:gh-pages
 
 cd -
